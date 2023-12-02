2b20dc9fde622617098a65c13806dba55d4a1609
Shanglei <Shangl@126.com>
2023-12-02 14:52:29 +1000

    EVCHANGING Signed-off-by: Shanglei <Shangl@126.com>

--- CodeADMM/compareL0_L1.m
+++ CodeADMM/compareL0_L1.m
@@ -1,13 +1,14 @@
-%主函数
+                                                                            %主函数
 close all
 clear
 
 %设置 模拟电车数量和时间剖分间隔
 N=2;dt=60/60;
-A=SmartGrid(N,dt);
+A=SmartGri
+d(N,dt);
 %设置各优化目标的权重 （总电费；总不满意度；削峰填谷效果；L0范数）
 
-A.W=[0.5,1,10,0.1].*[15/9,1,1,0.1];
+A.W=[1,0.2,1,0.01];
 %A.W=[0.5,1,10,0.1].*[15/9,1,1,0.11];
 %使用L0范数
 A.gg=1;
@@ -21,5 +22,5 @@ A.Solve();
 A.Show();
 
 %disp(A.P-B);
-disp('两个解的Linfty误差:');
+disp('The L-infinity difference with two solutions:');
 disp(max(max(A.P-B)));%
